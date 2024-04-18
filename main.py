b01fc428af6578c345b6ce3992d91df6898d78e6
Smart developer <developer@yandex.ru>
2024-04-18 18:15:29 +0500

    add imports and create def main()

---
+++ main.py
@@ -0,0 +1,14 @@
+from config import BOT_TOKEN
+import logging
+from telegram.ext import Application, MessageHandler, filters, ConversationHandler
+from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
