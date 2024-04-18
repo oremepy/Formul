7a238abb623f998802c8f0f26f43bea2a933aa7a
Smart developer <developer@yandex.ru>
2024-04-18 18:21:13 +0500

    add logging and create start(), close_keyboard() addition of main()

--- main.py
+++ main.py
@@ -6,7 +6,39 @@ from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
 
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+reply_keyboard = [['раздел1', 'раздел2'],
+                  ['раздел3']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+
+async def start(update, contexte):
+    user = update.effective_user
+    await update.message.reply_text(
+        rf"Привет {user.first_name}! Меня зовут Formul, и я помогу тебе найти нужную формулу. "
+        "В каком разделе находится нужная формула?",
+        reply_markup=markup
+    )
+    return 1
+
+
+async def close_keyboard(update, context):
+    await update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
 def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("close", close_keyboard))
+    application.run_polling()
     pass
 
 
