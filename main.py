60096703165eb2e2c0964c2a6c83cf08361cb924
Smart developer <developer@yandex.ru>
2024-04-18 21:34:26 +0500

    add many anything

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [['/help', '/start', '/show_formulas']]
+reply_keyboard = [['/help', '/start'],
+                  ['/show_formulas', '/tasks']]
+reply_key2 = [['/chapter1', '/chapter2', '/chapter3'],
+              ['/back']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+markup1 = ReplyKeyboardMarkup(reply_key2, one_time_keyboard=False)
 
 
 async def start(update, contexte):
@@ -23,24 +27,48 @@ async def start(update, contexte):
         "В каком разделе находится нужная формула?",
         reply_markup=markup
     )
-    return 1
 
 
 async def help(update, context):
     await update.message.reply_text(
         "Боту доступны следущие команды:\n"
         "/start\n"
-        "/help\n")
+        "/help\n"
+        "/close\n"
+        "/tasks\n"
+        "/show_formulas\n"
+        "/back\n")
 
 
 async def show_formulas(update, context):
     await update.message.reply_text(
-        "Выберете раздел в котором находится формула:")
+        "Выберете раздел в котором находится формула:",
+        reply_markup=markup1
+    )
+    return 1
 
 
 async def tasks(update, context):
     await update.message.reply_text(
         "Выберете тренировочное задание:")
+
+
+async def chapter1(update, context):
+    await update.message.reply_text(
+        "Ядерная физика")
+    return ConversationHandler.END
+
+
+async def chapter2(update, context):
+    await update.message.reply_text(
+        "Тепловые процессы")
+    return ConversationHandler.END
+
+
+async def chapter3(update, context):
+    await update.message.reply_text(
+        "Механика")
+    return ConversationHandler.END
 
 
 async def close_keyboard(update, context):
@@ -50,13 +78,51 @@ async def close_keyboard(update, context):
     )
 
 
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
+
+
+async def back(update, context):
+    await update.message.reply_text(
+        "Ok",
+        reply_markup=markup
+    )
+    return ConversationHandler.END
+
+
+conv_handler = ConversationHandler(
+    # Точка входа в диалог.
+    # В данном случае — команда /start. Она задаёт первый вопрос.
+    entry_points=[CommandHandler('show_formulas', show_formulas)],
+
+    # Состояние внутри диалога.
+    # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+    states={
+        # Функция читает ответ на первый вопрос и задаёт второй.
+        1: [MessageHandler(filters.TEXT & ~filters.COMMAND, chapter1)],
+        # Функция читает ответ на второй вопрос и завершает диалог.
+        2: [MessageHandler(filters.TEXT & ~filters.COMMAND, chapter2)],
+        3: [MessageHandler(filters.TEXT & ~filters.COMMAND, chapter3)]
+    },
+
+    # Точка прерывания диалога. В данном случае — команда /stop.
+    fallbacks=[CommandHandler('stop', stop)]
+)
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("show_formulas", show_formulas))
     application.add_handler(CommandHandler("tasks", tasks))
+    application.add_handler(CommandHandler("chapter1", chapter1))
+    application.add_handler(CommandHandler("chapter2", chapter2))
+    application.add_handler(CommandHandler("chapter3", chapter3))
+    application.add_handler(CommandHandler("back", back))
     application.add_handler(CommandHandler("close", close_keyboard))
+    application.add_handler(conv_handler)
     application.run_polling()
 
 
