ed895bed3b256430cf392a5a2f6a692304779262
Smart developer <developer@yandex.ru>
2024-04-18 18:57:00 +0500

    add help

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ from telegram.ext import CommandHandler
 from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
 
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [['раздел1', 'раздел2'],
-                  ['раздел3']]
+reply_keyboard = [['/help', '/start']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
 async def start(update, contexte):
     user = update.effective_user
     await update.message.reply_text(
-        rf"Привет {user.first_name}! Меня зовут Formul, и я помогу тебе найти нужную формулу. "
+        f"Привет {user.first_name}! Меня зовут Formul, и я помогу тебе найти нужную формулу.\n"
+        "Если ты не понимаешь как пользоваться ботом пиши: /help.\n"
         "В каком разделе находится нужная формула?",
         reply_markup=markup
     )
     return 1
+
+
+async def help(update, context):
+    await update.message.reply_text(
+        "Боту доступны следущие команды:\n"
+        "/start\n"
+        "/help\n")
 
 
 async def close_keyboard(update, context):
@@ -37,9 +43,9 @@ async def close_keyboard(update, context):
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("close", close_keyboard))
     application.run_polling()
-    pass
 
 
 if __name__ == '__main__':
